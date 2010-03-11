ae0d0622c53faf86e377a6ed76ad664621df7af7
László Pere <pipas@pipas.blumsoft.eu>
2010-03-11 10:21:33 +0100

    Names of private tags changed.

--- src/batteryapplet/timecontainer.cpp
+++ src/batteryapplet/timecontainer.cpp
@@ -1,3 +1,5 @@
+/* -*- Mode: C; indent-tabs-mode: s; c-basic-offset: 4; tab-width: 4 -*- */
+/* vim:set et ai sw=4 ts=4 sts=4: tw=80 cino="(0,W2s,i2s,t0,l1,:0" */
 #include "timecontainer.h"
 
 #include <DuiGridLayoutPolicy>
@@ -9,15 +11,17 @@ TimeContainer::TimeContainer(
 		const QString  &text, 
 		DuiImageWidget *image, 
 		DuiWidget      *parent) :
-	DuiContainer(parent),
-        image(image),
-        textLabel(NULL),
-        timeLabel(NULL)
+	DuiContainer (parent),
+        m_Image (image),
+        m_TextLabel (0),
+        m_TimeLabel (0)
 {
-    timeLabel = new DuiLabel ("N/A");
-    timeLabel->setObjectName ("batteryTimeLabel");
-    textLabel = new DuiLabel (text);
-    textLabel->setObjectName ("batteryLabel");
+    m_TimeLabel = new DuiLabel ("N/A");
+    m_TimeLabel->setObjectName ("batteryTimeLabel");
+
+    m_TextLabel = new DuiLabel (text);
+    m_TextLabel->setObjectName ("batteryLabel");
+
     setHeaderVisible (false);
     setLayout ();
 }
@@ -27,7 +31,7 @@ void
 TimeContainer::setText (
 		const QString &text)
 {
-    textLabel->setText (text);
+    m_TextLabel->setText (text);
 }
 
 void TimeContainer::updateTimeLabel (const QString &value)
@@ -35,11 +39,11 @@ void TimeContainer::updateTimeLabel (const QString &value)
     //% "%a hours %b minutes"
     const QString TimeValueText = qtTrId ("qtn_ener_remtime");
 
-    if (timeLabel == NULL)
+    if (m_TimeLabel == NULL)
         return;
 
     if (value.toInt() == 0) {// a text, not minute value
-        timeLabel->setText(value);
+        m_TimeLabel->setText(value);
         return;
     }
     int minutes = value.toInt();
@@ -58,7 +62,8 @@ void TimeContainer::updateTimeLabel (const QString &value)
         minsVar = (minsVar.toInt() == 0) ? "00" : minsVar;
         time = QString("%1:%2 %3").arg(minutes / 60).arg(minsVar.toString()).arg(hoursPrefix);
     }
-    timeLabel->setText(time);
+    
+    m_TimeLabel->setText(time);
 }
 
 void TimeContainer::setLayout()
@@ -69,8 +74,8 @@ void TimeContainer::setLayout()
     centralWidget()->setLayout(layout);
 
     // add the widgets
-    layoutPolicy->addItem(image, 0, 0, 2, 1);
-    layoutPolicy->addItem(textLabel, 0, 1, 1, 1);
-    layoutPolicy->addItem(timeLabel, 1, 1, 1, 1);
+    layoutPolicy->addItem (m_Image, 0, 0, 2, 1);
+    layoutPolicy->addItem (m_TextLabel, 0, 1, 1, 1);
+    layoutPolicy->addItem (m_TimeLabel, 1, 1, 1, 1);
 }
 
