cc61f24fa693002a274a06eaadf5476e5245eefa
László Pere <pipas@pipas.blumsoft.eu>
2010-03-31 09:33:12 +0200

    Enhanced ThemeBusinessLogic unit tests (libdui has a bugfix).

--- tests/ut_themebusinesslogic/ut_themebusinesslogic.cpp
+++ tests/ut_themebusinesslogic/ut_themebusinesslogic.cpp
@@ -9,6 +9,18 @@
 #define DEBUG
 #include "../../src/debug.h"
 
+static const int themeWaitMSec = 1000;
+
+/******************************************************************************
+ * Ut_ThemeBusinessLogicPrivate implementation. 
+ */
+void
+UtThemeBusinessLogicPrivate::themeChanged (
+        QString themeCodeName)
+{
+    m_ThemeCodeName = themeCodeName;
+}
+
 /******************************************************************************
  * Ut_ThemeBusinessLogic implementation. 
  */
@@ -29,8 +41,12 @@ Ut_ThemeBusinessLogic::initTestCase()
     int argc = 1;
     char* app_name = (char*) "./Ut_ThemeBusinessLogic";
 
+    m_Priv = new UtThemeBusinessLogicPrivate;
     m_App = new DuiApplication (argc, &app_name);
     m_Api = new ThemeBusinessLogic;
+
+    connect (m_Api, SIGNAL(themeChanged(QString)),
+            m_Priv, SLOT(themeChanged(QString)));
 }
 
 void 
@@ -38,8 +54,12 @@ Ut_ThemeBusinessLogic::cleanupTestCase()
 {
     delete m_App;
     delete m_Api;
+    delete m_Priv;
 }
 
+/*!
+ * Checks if the information on the current theme is available and valid.
+ */
 void
 Ut_ThemeBusinessLogic::testCurrentTheme ()
 {
@@ -54,6 +74,11 @@ Ut_ThemeBusinessLogic::testCurrentTheme ()
     QVERIFY (!value.isEmpty());
 }
 
+/*!
+ * Checks if there are available themes (at least one) and checks if the name,
+ * icon name and codename is not an empty string for all the visible themes.
+ * Moreover all the available themes must be valid.
+ */
 void
 Ut_ThemeBusinessLogic::testAvailableThemes ()
 {
@@ -75,6 +100,30 @@ Ut_ThemeBusinessLogic::testAvailableThemes ()
             QVERIFY (!desc->codeName().isEmpty());
             QVERIFY (!desc->iconName().isEmpty());
         }
+    }
+}
+
+/*!
+ * Takes all the themes that are visible and changes the current theme to this
+ * themes one by one. Checks if the themeChanged() signal is arrived with this
+ * new theme.
+ */
+void
+Ut_ThemeBusinessLogic::testChangeTheme ()
+{
+    QList<ThemeDescriptor *> list = m_Api->availableThemes ();
+
+    foreach (ThemeDescriptor *desc, list) {
+        if (!desc->isVisible()) {
+            continue;
+        }
+
+        qDebug() << "Changing theme to " << desc->codeName();
+        m_Api->changeTheme (desc->codeName());
+        QTest::qWait (themeWaitMSec);
+
+        qDebug() << "Theme is          " << m_Priv->m_ThemeCodeName;
+        QVERIFY (m_Priv->m_ThemeCodeName == desc->codeName());
     }
 }
 
