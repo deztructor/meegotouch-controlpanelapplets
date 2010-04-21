7aa590b4c014b612bbb62114726987a115a7bbea
László Pere <lpere@blumsoft.eu>
2010-04-21 10:49:10 +0200

    Minor changes.

--- src/resetapplet/resetbusinesslogic.cpp
+++ src/resetapplet/resetbusinesslogic.cpp
@@ -2,12 +2,26 @@
 /* vim:set et ai sw=4 ts=4 sts=4: tw=80 cino="(0,W2s,i2s,t0,l1,:0" */
 
 #include "resetbusinesslogic.h"
+#include <QString>
+#include <MGConfItem>
 
 #define DEBUG
 #include "../debug.h"
 
+static const QString autolockGConKey = 
+    "/system/osso/dsm/locks/devicelock_autolock_timeout";
+
 ResetBusinessLogic::ResetBusinessLogic()
 {
+    MGConfItem   gconfItem (autolockGConKey);
+    QString      value = gconfItem.value().toString();
+
+    /*
+     * Well, I'm not sure how to interpret this value, but the 
+     * duicontrolpanel-devicelockapplet is not available for my meego target 
+     * yet.
+     */
+    SYS_DEBUG ("*** autolockValue = %s", SYS_STR(value));
 }
 
 ResetBusinessLogic::~ResetBusinessLogic()
@@ -15,7 +29,7 @@ ResetBusinessLogic::~ResetBusinessLogic()
 }
 
 /*!
- * Performs teh 'Restore original settings' action.
+ * Performs the 'Restore original settings' action.
  */
 void
 ResetBusinessLogic::performRestoreSettings ()
