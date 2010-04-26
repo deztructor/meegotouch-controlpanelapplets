8b9f717d9d40d79e20be6341e1ba20f188dc2808
László Pere <lpere@blumsoft.eu>
2010-04-26 11:14:15 +0200

    themeapplet rfs

--- src/themeapplet/theme-rfs.sh
+++ src/themeapplet/theme-rfs.sh
@@ -4,5 +4,7 @@
 # The 'reset factory settings' script for the theme applet.
 #
 
+# FIXME: Nothing seems to work...
+#gconftool-2 --type string --set /meegotouch/theme/name ""
 gconftool-2 --recursive-unset /meegotouch/theme/name
 
