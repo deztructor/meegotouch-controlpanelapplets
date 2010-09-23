336501c27c89307f0f76b677dba88f6e3c9aae03
László Pere <lpere@blumsoft.eu>
2010-09-23 09:30:42 +0200

    Added copyright note for wallpaper headers.

--- src/wallpaperapplet/wallpapergconf.h
+++ src/wallpaperapplet/wallpapergconf.h
@@ -1,13 +1,13 @@
-/* -*- Mode: C; indent-tabs-mode: s; c-basic-offset: 4; tab-width: 4 -*- */
-/* vim:set et sw=4 ts=4 sts=4: */
-/***************************************************************************
+/****************************************************************************
 **
 ** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
 ** All rights reserved.
-** Contact: Karoliina T. Salminen <karoliina.t.salminen@nokia.com>
+** Contact: Nokia Corporation (directui@nokia.com)
 **
-** This file is part of duicontrolpanel.
+** This file is part of meegotouch-controlpanelapplets.
 **
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
 **
 ** This library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU Lesser General Public
