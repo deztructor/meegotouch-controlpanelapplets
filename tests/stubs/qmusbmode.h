b441cbd28e43f29bea319cc810fec223584ca46b
László Pere <lpere@blumsoft.eu>
2010-09-23 10:02:04 +0200

    Added copyright notice to tests.

--- tests/stubs/qmusbmode.h
+++ tests/stubs/qmusbmode.h
@@ -1,5 +1,3 @@
-/* -*- Mode: C; indent-tabs-mode: s; c-basic-offset: 4; tab-width: 4 -*- */
-/* vim:set et sw=4 ts=4 sts=4: */
 /****************************************************************************
 **
 ** Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
