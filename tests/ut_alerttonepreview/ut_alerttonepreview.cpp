ee8b6e77c9a4f80121b91f139dcfc0f11463cf1d
David Kedves <dkedves@blumsoft.eu>
2011-02-24 13:12:55 +0100

    Changes: fix for ut_alertonevolume

--- tests/ut_alerttonepreview/ut_alerttonepreview.cpp
+++ tests/ut_alerttonepreview/ut_alerttonepreview.cpp
@@ -1,5 +1,23 @@
-/* -*- Mode: C; indent-tabs-mode: s; c-basic-offset: 4; tab-width: 4 -*- */
-/* vim:set et sw=4 ts=4 sts=4: */
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** This file is part of meegotouch-controlpanelapplets.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
+#include "ut_alerttonepreview.h"
+#include "alerttonepreview.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -7,20 +25,15 @@
 #include <MApplicationWindow>
 #include <QString>
 #include <QStringList>
-#include  "ut_alerttonepreview.h"
-#include  "alerttonepreview.h"
 
 #include "qprofilevalue.h"
-
-#ifdef HAVE_LIBRESOURCEQT
-#include <policy/resource-set.h>
-#endif
 
 /******************************************************************************
  * Stubs
  */
 
 #ifdef HAVE_LIBRESOURCEQT
+#include <policy/resource-set.h>
 namespace ResourcePolicy {
 //ResourceSet::ResourceSet(const QString &applicationClass, QObject *parent)
 //{
