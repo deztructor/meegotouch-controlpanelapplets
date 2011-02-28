39cf7bbeebb92555816ca1eb38660d8f0a5b845c
David Kedves <dkedves@blumsoft.eu>
2011-02-28 11:46:32 +0100

    Fixes: a failing test-case due to the previous changes...

--- tests/ut_qgconfvalue/ut_qgconfvalue.cpp
+++ tests/ut_qgconfvalue/ut_qgconfvalue.cpp
@@ -1,5 +1,21 @@
-/* -*- Mode: C; indent-tabs-mode: s; c-basic-offset: 4; tab-width: 4 -*- */
-/* vim:set et sw=4 ts=4 sts=4: */
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (directui@nokia.com)
+**
+** This file is part of meegotouch-controlpanelapplets.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at directui@nokia.com.
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation
+** and appearing in the file LICENSE.LGPL included in the packaging
+** of this file.
+**
+****************************************************************************/
 
 #include <MApplication>
 #include <MApplicationWindow>
@@ -111,7 +127,8 @@ Ut_QGConfValueTests::qgconfvalueConstructor ()
 
       QCOMPARE(qgcv.m_lsDir[0], QString("meegotouch"));
       QCOMPARE(qgcv.m_lsDir[1], QString("input_feedback"));
-      QCOMPARE(qgcv.m_lsDir[2], QString("volume"));
+      QCOMPARE(qgcv.m_lsDir[2], QString("2"));
+      QCOMPARE(qgcv.m_lsDir[3], QString("volume"));
 }
 
 /*!
