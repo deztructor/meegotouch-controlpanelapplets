003d7b43b01d46012a237147b4708e91f88379dc
László Pere <lpere@blumsoft.eu>
2011-04-13 13:14:21 +0200

    Fixed: NB243944 - Too large title font used in Display settings view

--- src/displayapplet/displaywidget.cpp
+++ src/displayapplet/displaywidget.cpp
@@ -79,7 +79,7 @@ void DisplayWidget::initWidget ()
      */
     // Row 1: The title label
     addHeaderContainer ();
-    //addStretcher ("CommonHeaderDividerInverted");
+    addStretcher ("CommonHeaderDividerInverted");
     //addSecHeaderContainer ();
     //addSliderContainer ();
     //addStretcher ("CommonItemDivider");
@@ -105,7 +105,7 @@ DisplayWidget::addHeaderContainer ()
     //% "Display"
     m_TitleLabel = new MLabel (qtTrId ("qtn_disp_display"));
     m_TitleLabel->setObjectName ("TitleLabel");
-    m_TitleLabel->setStyleName ("CommonApplicationHeaderInverted");
+    m_TitleLabel->setStyleName ("CommonHeaderInverted");
     layout->addItem (m_TitleLabel);
     layout->setAlignment (m_TitleLabel, Qt::AlignLeft);
     /*
