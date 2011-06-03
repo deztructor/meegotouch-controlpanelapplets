ceb91bd5f9659d7a2255533cc202cadb8e98c9e2
David Kedves <dkedves@blumsoft.eu>
2011-06-03 13:04:11 +0200

    Changes: wallpaperapplet2 .deb building can be enabled trought the DEB_BUILD_OPTIONS=wallpaper2 environment var.

--- gen_wallpaper_install.sh
+++ gen_wallpaper_install.sh
@@ -7,10 +7,15 @@ INSTALL_FILE=debian/duicontrolpanel-wallpaperapplet.install
 
 LIBNAME=libwallpaperapplet
 DESKTOPNAME=wallpaper
+echo '##########################################################'
 if [ "$1x" == "improvedx" ]; then
     LIBNAME=libwallpaperapplet2
     DESKTOPNAME=wallpaper2
+    echo '##### Building package with the NEW wallpaper applet #####'
+else
+    echo '##### Building package with the old wallpaper applet #####'
 fi
+echo '##########################################################'
 
 cat - > ${INSTALL_FILE} <<CONTENTS
 usr/share/themes/base/meegotouch/libwallpaperapplet/style/${LIBNAME}.css
