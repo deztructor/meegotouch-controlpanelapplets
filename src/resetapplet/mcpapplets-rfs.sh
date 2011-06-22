e3ca19b80b6b9496b7bd3ceda534e754750ea9a9
David Kedves <dkedves@blumsoft.eu>
2011-06-22 13:39:28 +0300

    Changes: remove mcompositor.conf in mcpapplets-rfs.sh

--- src/resetapplet/mcpapplets-rfs.sh
+++ src/resetapplet/mcpapplets-rfs.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
+set -e
 # USB applet
 gconftool-2 --recursive-unset /Meego/System/UsbMode
 # Display settings applet
 gconftool-2 --recursive-unset /system/osso/dsm/display
+rm -f $HOME/.config/mcompositor/mcompositor.conf
 # Battery settings applet
 gconftool-2 --recursive-unset /system/osso/dsm/energymanagement
 # Wallpaper settings applet
