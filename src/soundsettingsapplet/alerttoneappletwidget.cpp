333835f933e28fc36c40820e1f2451ab1f96d118
David Kedves <dkedves@blumsoft.eu>
2011-07-05 12:07:23 +0300

    Changes: Soundsettingsapplet: do some meego ifdeds

--- src/soundsettingsapplet/alerttoneappletwidget.cpp
+++ src/soundsettingsapplet/alerttoneappletwidget.cpp
@@ -104,6 +104,7 @@ AlertToneAppletWidget::createContents()
 	mainLayout->setLandscapePolicy(policy);
 	mainLayout->setPortraitPolicy(policy);
 
+#ifndef MEEGO
     /*
      * The main title
      */
@@ -111,6 +112,7 @@ AlertToneAppletWidget::createContents()
             centralWidget, policy,
             APP_TITLE_LABEL_STYLE_NAME); 
     label->setText(qtTrId("qtn_sond_sounds"));
+#endif
 
 #if 0
     MSeparator            *spacer;
